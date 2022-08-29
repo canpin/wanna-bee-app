f6f6fb3cde95be69b15cad7ffc63ab16a284b9cd
canpin <tomi.kamppinen@gmail.com>
2022-08-29 11:04:35 +0300

    Don't allow duplicate heros

--- server/controllers/superheros.ts
+++ server/controllers/superheros.ts
@@ -2,26 +2,33 @@ import { Response, Request } from "express";
 import ISuperPower from "../../API/ISuperPower";
 import ISuperHero from "../../API/ISuperHero";
 
-/*let superheroNames: String[] = [
-
-]*/
+const superheroNames: String[] = ["SUPERMAN"];
 
 export const getSuperPowers = async (req: Request, res: Response): Promise<void> => {
-  const superpowers: ISuperPower[] = [{ ability: "Fly"}, { ability: "Walk through walls"}, { ability: "X-ray vision"}];
+  const superpowers: ISuperPower[] = [{ ability: "Flying"}, { ability: "Walking through walls"}, { ability: "X-ray vision"}];
   res.status(200).json(superpowers);
 }
 
 export const addSuperHero =async (req: Request, res: Response): Promise<void> => {
   const newSuperHero = req.body;
+  const name: string = newSuperHero.name;
+
   let status: number = 200;
-  let message: string = `You can be ${newSuperHero.name}!`;
-  if (!newSuperHero.name) {
+  let message: string = `You can be ${name}.`;
+  if (!name) {
     status = 400;
     message = "Super hero needs a name!";
   }
   else if (!newSuperHero.superPower?.ability) {
     status = 400;
     message = "Super hero needs a super power!";
+  }
+  else if (superheroNames.includes(name.toUpperCase())) {
+    status = 400;
+    message = `There can only be one ${name}!`;
+  }
+  else {
+    superheroNames.push(name.toUpperCase())
   }
   res.status(status).send(message);
 }
